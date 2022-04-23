f3aaf25f90bfe0093234e36aeaa1d11935bc5962
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-04-23 01:59:36 -0700

    before brian design

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import './navbar.css'
 
+import linkedlogo from '../../assets/linkedinlogo.png'
+import githublogo from '../../assets/githublogo.png'
+
 function Navbar() {
     return (
         <div className="navWrapper">
-            <div className="navLogo">
+            <div className="navLeft">
                 ben {"&"} queries.
             </div>
+
+            <div className="navCenter">
+                <span>ABOUT ME</span>
+                <span>PROJECTS</span>
+                <span>RESUME</span>
+            </div>
             
+            <div className="navRight">
+                <img className="githubLogo" src={githublogo} alt="" />
+                <img className="linkedLogo" src={linkedlogo} alt="" />
+
+            </div>
 
         </div>
     )
