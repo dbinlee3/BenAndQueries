b51688aceca699ae5408360999c533bc187dc7c8
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-04-22 23:36:26 -0700

    resetup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-
+import './App.css';
+import Navbar from './components/navbar/Navbar.js'
 
 function App() {
   return (
     <>
-      Hello.
+      <Navbar />
     </>
   );
 }
---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navWrapper">
+            <div className="navLogo">
+                ben {"&"} queries.
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default Navbar
