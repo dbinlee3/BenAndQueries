e9cdf07bd9969290dfeb31e69781da6e2db35893
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-04-23 13:42:25 -0700

    before experimenting

---
+++ frontend/src/components/navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'ABOUT ME',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'PROJECTS',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'RESUME',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+
+]
