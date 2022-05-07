a24693be461acc718fa88d26134c6e428e948294
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-05-07 01:34:39 -0700

    done with projects page

---
+++ frontend/src/pages/projects/descriptions/cplusplus.js
@@ -0,0 +1,3 @@
+const CPLUSPLUS = "This is a program I wrote in C++ in February of 2022. It takes in a file and uses efficient encoding in order to compress the file into bits. This project covers advanced data structure topics such as the Huffman Tree data structure, priority queues, and an efficient encoding algorithm to compress and decompress a given file."
+
+export default CPLUSPLUS;
